258cc2805d3f2c705a71338e63fbd0f0cef83d96
Caique Santana <caique.santana10.cs@gmail.com>
2020-03-24 22:03:18 -0300

    Add wrapper to chrome.storage to save/get data without callbacks

--- background.js
+++ background.js
@@ -1,4 +1,27 @@
-chrome.browserAction.onClicked.addListener(function () {
-  // TODO: handle extension's icon
+const storage = {
+  set: function (data) {
+    return new Promise((resolve) => {
+      chrome.storage.local.set(data, function () {
+        resolve()
+      })
+    })
+  },
+  get: function (key) {
+    return new Promise((resolve) => {
+      chrome.storage.local.get([key], function (response) {
+        resolve(response)
+      })
+    })
+  }
+}
+
+chrome.browserAction.onClicked.addListener(async function () {
+  const value = await storage.get('active')
+
+  await storage.set({ active: !value.active })
+})
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+
 })
 
